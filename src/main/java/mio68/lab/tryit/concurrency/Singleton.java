275ccd19573567e83a65d1521926cc6e26443e50
Igor Markivskiy <mio6823@gmail.com>
2022-02-23 19:21:28 +0700

    Singleton with double-check-locking.

---
+++ src/main/java/mio68/lab/tryit/concurrency/Singleton.java
@@ -0,0 +1,24 @@
+package mio68.lab.tryit.concurrency;
+
+/*
+ *   https://www.baeldung.com/java-singleton-double-checked-locking
+ */
+public class Singleton {
+
+    private static volatile Singleton INSTANCE;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (INSTANCE == null) {
+            synchronized (Singleton.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new Singleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
