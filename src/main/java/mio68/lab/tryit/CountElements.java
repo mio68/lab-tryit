c02edfc86234eb7f3e48695beb6acd877f57e899
Igor Markivskiy <mio6823@gmail.com>
2022-08-20 12:02:46 +0700

    Java 9 map initialization style

--- src/main/java/mio68/lab/tryit/CountElements.java
+++ src/main/java/mio68/lab/tryit/CountElements.java
@@ -1,33 +1,33 @@
 package mio68.lab.tryit;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CountElements {
 
-    public static <T> Map<T, Long> countFrquencyOfElements(List<T> list) {
+    public static <T> Map<T, Long> countFrequencyOfElements(List<T> list) {
         return list.stream()
                 .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
     }
 
-    private final static Map<Integer, String> intToWordTranslator = new HashMap<>();
-    static {
-        intToWordTranslator.put(1, "one");
-        intToWordTranslator.put(2, "two");
-        intToWordTranslator.put(3, "three");
-    }
-
+    // Immutable map
+    private final static Map<Integer, String> intToWordTranslator = Map.of(
+            1, "one",
+            2, "two",
+            3, "three"
+    );
 
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(1,2,3,2,3,1,2,2,1,3,2,3);
         List<String> stringList = integerList.stream()
-                .map(i -> intToWordTranslator.get(i))
-                        .collect(Collectors.toList());
+                .map(intToWordTranslator::get)
+                .collect(Collectors.toList());
 
-        System.out.println(countFrquencyOfElements(integerList));
-        System.out.println(countFrquencyOfElements(stringList));
+        System.out.println(countFrequencyOfElements(integerList));
+        System.out.println(countFrequencyOfElements(stringList));
+
+//        intToWordTranslator.put(4, "four"); // throws UnsupportedOperationException
     }
 }
