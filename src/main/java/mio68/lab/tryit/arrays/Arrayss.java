c4bd62428c2cd668792631b1ed2ee2f2925b21eb
Igor Markivskiy <mio6823@gmail.com>
2022-08-26 15:48:48 +0700

    learn array initialization

--- src/main/java/mio68/lab/tryit/arrays/Arrayss.java
+++ src/main/java/mio68/lab/tryit/arrays/Arrayss.java
@@ -1,5 +1,7 @@
 package mio68.lab.tryit.arrays;
 
+import java.util.Arrays;
+
 public class Arrayss {
 
     public static void main(String[] args) {
@@ -7,10 +9,20 @@ public class Arrayss {
         Object[] objects = integers; // it's ok because arrays are covariant
 //        objects[0] = "Try to put a string into array of integers!"; // ArrayStoreException at runtime!
 
-        Long[] longs = new Long[1];
-
+        Long[] longs = new Long[10];
         // Compilation error - incompatible types: java.lang.Object[] cannot be converted to java.lang.Long[]
 //        longs = objects;
+
+
+        String[] strings = new String[3];
+        System.out.println("array of strings is initialized: " + Arrays.toString(strings));
+
+        int[] ints = new int[3];
+        System.out.println("array of int initialized: " + Arrays.toString(ints));
+
+        boolean[] booleans = new boolean[3];
+        System.out.println("array of booleans is initialized: " + Arrays.toString(booleans));
+
 
     }
 }
