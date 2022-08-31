c33b1d02d5ec78323902e3cbd974470f4c9d1ca6
Igor Markivskiy <mio6823@gmail.com>
2022-08-31 09:44:45 +0700

    commented

---
+++ src/main/java/mio68/lab/tryit/realnumbers/NanComparing.java
@@ -0,0 +1,8 @@
+package mio68.lab.tryit.realnumbers;
+
+public class NanComparing {
+
+    public static void main(String[] args) {
+        System.out.println(Float.NaN == Float.NaN); // Prints false
+    }
+}
