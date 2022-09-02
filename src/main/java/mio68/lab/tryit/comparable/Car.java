ef300009198c3514519e17d3f47b16e2b4b34e12
Igor Markivskiy <mio6823@gmail.com>
2022-09-02 12:49:35 +0700

    learn comparable

---
+++ src/main/java/mio68/lab/tryit/comparable/Car.java
@@ -0,0 +1,70 @@
+package mio68.lab.tryit.comparable;
+
+import lombok.ToString;
+
+import java.util.*;
+
+@ToString
+public class Car implements Comparable<Car> {
+
+    private static final Comparator<Car> COMPARATOR =
+            Comparator.comparing(Car::getManufacturer)
+                    .thenComparing(Car::getModel)
+                    .thenComparing((Car c) -> c.modification,
+                            Comparator.nullsFirst(String::compareTo));
+
+    private final String manufacturer;
+    private final String model;
+    // modification can be null
+    private final String modification;
+
+    public Car(String manufacturer, String model, String modification) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.modification = modification;
+    }
+
+    public Car(String manufacturer, String model) {
+        this(manufacturer, model, null);
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Optional<String> getModification() {
+        return Optional.ofNullable(modification);
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return COMPARATOR.compare(this, o);
+    }
+
+    public static void main(String[] args) {
+        List<Car> products = Arrays.asList(
+                new Car("Ford","Fiesta"),
+                new Car("Toyota","Yaris"),
+                new Car("Toyota","Mark2"),
+                new Car("Ford","Focus","V2"),
+                new Car("Toyota","Camri"),
+                new Car("Ford","Focus"),
+                new Car("Ford","Focus","V3"),
+                new Car("Ford","Focus","V2-restyling"),
+                new Car("Toyota","Corolla"),
+                new Car("Ford","Mondeo"),
+                new Car("Toyota","Avensis"),
+                new Car("Toyota","Surf"),
+                new Car("Mazda","Demio"));
+
+        Collections.sort(products);
+
+        products.forEach(System.out::println);
+    }
+
+
+}
---
+++ src/main/java/mio68/lab/tryit/comparable/Product.java
@@ -0,0 +1,68 @@
+package mio68.lab.tryit.comparable;
+
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+@ToString
+public class Product implements Comparable<Product> {
+
+    private static Comparator<String> MODIFICATION_COMPARATOR =
+            Comparator.nullsFirst(String::compareTo);
+
+    private final String manufacturer;
+    private final String model;
+    // modification can be null
+    private final String modification;
+
+
+    public Product(String manufacturer, String model, String modification) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.modification = modification;
+    }
+
+    public Product(String manufacturer, String model) {
+        this(manufacturer, model, null);
+    }
+
+    @Override
+    public int compareTo(Product o) {
+        int result = manufacturer.compareTo(o.manufacturer);
+        if (result == 0) {
+            result = model.compareTo(o.model);
+            if (result == 0) {
+                result = MODIFICATION_COMPARATOR.compare(
+                        modification,
+                        o.modification);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Product> products = Arrays.asList(
+                new Product("Ford","Fiesta"),
+                new Product("Toyota","Yaris"),
+                new Product("Toyota","Mark2"),
+                new Product("Ford","Focus","V2"),
+                new Product("Toyota","Camri"),
+                new Product("Ford","Focus"),
+                new Product("Ford","Focus","V3"),
+                new Product("Ford","Focus","V2-restyling"),
+                new Product("Toyota","Corolla"),
+                new Product("Ford","Mondeo"),
+                new Product("Toyota","Avensis"),
+                new Product("Toyota","Surf"),
+                new Product("Mazda","Demio"));
+
+        Collections.sort(products);
+
+        products.forEach(System.out::println);
+    }
+
+
+}
