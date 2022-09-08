044c7d9a2ca07e2f740d428511e57d7b7f25d4f7
Igor Markivskiy <mio6823@gmail.com>
2022-09-08 21:40:36 +0700

    learning enums - what exception is thrown by client when enum element is removed

--- src/main/java/mio68/lab/tryit/model/NobleGas.java
+++ src/main/java/mio68/lab/tryit/model/NobleGas.java
@@ -4,6 +4,8 @@ public enum NobleGas {
 
     HELIUM(2),
     NEON(10),
+// If element of enum is removed than client class that still uses it will throw at runtime:
+// Exception in thread "main" java.lang.NoSuchFieldError: ARGON
     ARGON(18),
     KRYPTON(36),
     XENON(54),
