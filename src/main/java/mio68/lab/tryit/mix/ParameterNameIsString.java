e153bbfd0ca5dae8c3cba8886c0bee04fa27c82c
Igor Markivskiy <mio6823@gmail.com>
2022-09-15 21:31:23 +0700

    strange java

---
+++ src/main/java/mio68/lab/tryit/mix/ParameterNameIsString.java
@@ -0,0 +1,16 @@
+package mio68.lab.tryit.mix;
+
+public class ParameterNameIsString {
+
+    public static void doIt(String String) { //1
+        int i = 10;
+        i : for (int k = 0 ; k< 10; k++) { //2
+            System.out.println( String + i); //3
+            if( k*k > 10) continue i; //4
+        }
+    }
+
+    public static void main(String[] args) {
+        doIt("Hi ");
+    }
+}
