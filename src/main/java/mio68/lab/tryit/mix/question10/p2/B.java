a6272f300066ee26c3b72e4f0f45130864517ab8
Igor Markivskiy <mio6823@gmail.com>
2022-09-16 13:56:25 +0700

    question #10 commented

--- src/main/java/mio68/lab/tryit/mix/question10/p2/B.java
+++ src/main/java/mio68/lab/tryit/mix/question10/p2/B.java
@@ -6,6 +6,7 @@ public class B extends A{
     public void process(A a) {
         //Compilation error
         // java: i has protected access in mio68.lab.tryit.mix.question10.p1.A
+        // If A is moved to package p2 - then it's field i is accesible by B
 //        a.i = a.i*2;
     }
     public static void main(String[] args) {
