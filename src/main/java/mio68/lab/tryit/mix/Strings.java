5cc27598c2f69cb9e84f28997726188d23577cbf
Igor Markivskiy <mio6823@gmail.com>
2022-10-17 13:15:40 +0700

    learning String

---
+++ src/main/java/mio68/lab/tryit/mix/Strings.java
@@ -0,0 +1,11 @@
+package mio68.lab.tryit.mix;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String s = "Hello World!";
+
+        System.out.println(String.format("s.substring(0,0) = [%s]",s.substring(0,0)));
+    }
+
+}
