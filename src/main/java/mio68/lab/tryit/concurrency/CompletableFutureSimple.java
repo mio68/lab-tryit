93cfd3a957d245dfe5bd7a859f9d8d761bcfba88
Igor Markivskiy <mio6823@gmail.com>
2022-10-23 22:30:44 +0700

    CompletableFuture supplier with unchecked exception

--- src/main/java/mio68/lab/tryit/concurrency/CompletableFutureSimple.java
+++ src/main/java/mio68/lab/tryit/concurrency/CompletableFutureSimple.java
@@ -14,12 +14,29 @@ public class CompletableFutureSimple {
         System.out.println("Got another word " + future.get());
 
         executorService.shutdown();
+
+        System.out.println("Got word " + getWordExceptionally(false).get());
+
+        System.out.println("Try to say never!");
+        System.out.println("Got word " + getWordExceptionally(true).get());
+
     }
 
 
     public static CompletableFuture<String> getWord() {
         // Supplier doesn't allow exception throwing
         return CompletableFuture.supplyAsync(() -> "Hello");
+    }
+
+    public static CompletableFuture<String> getWordExceptionally(boolean throwException) {
+        // Supplier doesn't allow exception throwing
+        return CompletableFuture.supplyAsync(() -> {
+            if (throwException) {
+                throw new RuntimeException("Exception throwing is requested!");
+            } else {
+                return "Never say never again!";
+            }
+        });
     }
 
     public static Future<String> getWordExceptionally(ExecutorService executorService) {
