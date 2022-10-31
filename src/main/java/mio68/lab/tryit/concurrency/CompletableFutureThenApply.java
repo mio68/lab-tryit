747cf4cb1147135f1b032fa7fbaf812eece90e9d
Igor Markivskiy <mio6823@gmail.com>
2022-10-31 11:43:49 +0700

    learnig CompletableFuture thenApply method

--- src/main/java/mio68/lab/tryit/concurrency/CompletableFutureThenApply.java
+++ src/main/java/mio68/lab/tryit/concurrency/CompletableFutureThenApply.java
@@ -3,15 +3,25 @@ package mio68.lab.tryit.concurrency;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class CompletableFutureThenApply {
 
     public static void main(String[] args) throws InterruptedException {
+
+        thenApplyForCompleted();
+        thenApplyForUncompleted();
+        thenApplyForCompletedExceptionally();
+
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    public static void thenApplyForCompleted() {
         CompletableFuture<String> completed = CompletableFuture.completedFuture("completed");
 
-        // thenApply executes in the main thread
+        // thenApply executes in the main thread, because it's already completed
         completed.thenApply((s) -> {
                     log.info("thenApply for initially completed");
                     return s + ".thenApply";
@@ -25,9 +35,15 @@ public class CompletableFutureThenApply {
                 }
         );
 
+    }
+
+    public static void thenApplyForUncompleted() {
         CompletableFuture<String> uncompleted = new CompletableFuture<>();
 
-        // thenApply will be executed by client thread
+        // thenApply will be executed:
+        // if it's completed then executes by main thread
+        // otherwise it returns completable future to main thread and
+        // actually will execute function of thread that completes original CF
         uncompleted.thenApply((s) -> {
                     log.info("thenApply for initially uncompleted");
                     return s + ".thenApply";
@@ -41,9 +57,37 @@ public class CompletableFutureThenApply {
                 }
         );
 
-        uncompleted.complete("completed by main thread"); // supplier of thenApply will be executed by main thread
+        // supplier of thenApply will be executed by main thread,
+        // because it's the thread that completes the CF!
+        uncompleted.complete("completed by main thread");
 
-        TimeUnit.SECONDS.sleep(5);
     }
 
+/*
+    This prints:
+    java.util.concurrent.ExecutionException: java.lang.Exception: completed with Exception
+    at java.base/java.util.concurrent.CompletableFuture.reportGet(CompletableFuture.java:395)
+    at java.base/java.util.concurrent.CompletableFuture.get(CompletableFuture.java:2069)
+    at mio68.lab.tryit.concurrency.CompletableFutureThenApply.thenApplyForCompletedExceptionally(CompletableFutureThenApply.java:77)
+    at mio68.lab.tryit.concurrency.CompletableFutureThenApply.main(CompletableFutureThenApply.java:16)
+    Caused by: java.lang.Exception: completed with Exception
+    at mio68.lab.tryit.concurrency.CompletableFutureThenApply.thenApplyForCompletedExceptionally(CompletableFutureThenApply.java:68)
+            ... 1 more
+*/
+    public static void thenApplyForCompletedExceptionally() {
+        CompletableFuture<String> completedExceptionally = new CompletableFuture<>();
+        completedExceptionally.completeExceptionally(new Exception("completed with Exception"));
+
+        CompletableFuture<String> then = completedExceptionally.thenApplyAsync(
+                (s) -> {
+                    log.info("thenApplyAsync for completed exceptionally");
+                    return s + ".then";
+                });
+
+        try {
+            System.out.println(then.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
