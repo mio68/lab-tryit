bfe2f5ada6bcabe7547442cd81c68d46da7f643d
Igor Markivskiy <mio6823@gmail.com>
2023-01-13 19:12:20 +0700

    expressions

---
+++ src/main/java/mio68/lab/tryit/expression/Exp1.java
@@ -0,0 +1,9 @@
+package mio68.lab.tryit.expression;
+
+public class Exp1 {
+    public static void main(String[] args) {
+        int i;
+        System.out.println((i=123));
+        System.out.println(i);
+    }
+}
