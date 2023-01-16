d340fd2faa09319adef0aa7a222ab220004741ec
Igor Markivskiy <mio6823@gmail.com>
2023-01-16 18:07:02 +0700

    UTF-8 and Strings

--- src/main/java/mio68/lab/tryit/Strings/CaterpillarLength.java
+++ src/main/java/mio68/lab/tryit/Strings/CaterpillarLength.java
@@ -1,20 +1,36 @@
 package mio68.lab.tryit.Strings;
 
-// https://habr.com/ru/company/jugru/blog/709952/
 
+/*
+ https://habr.com/ru/company/jugru/blog/709952/
+ UTF-8 использует переменное количество байт на один символ — от 1 до 4.
+ UTF-16 кодирует количеством байтов кратным 2.
+ UTF-32 фиксированной длины — всегда 4 байта.
+
+
+*/
 public class CaterpillarLength {
 
     public static void main(String[] args) {
         String caterpillar = "🐛";
         System.out.println(caterpillar.length()); // 2
+        System.out.println(toStringOfHex(caterpillar));
 
-        char[] chars = caterpillar.toCharArray(); // '\uD83D', '\uDC1B'
-
-
-
-        for (char c : chars) {
-            System.out.println(Integer.toHexString(c));
-        }
-
+        String greet = "Привет Joker";
+        System.out.println(toStringOfHex(greet));
     }
+
+    static String toStringOfHex(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < chars.length; i++) {
+            if (i != 0) {
+                sb.append(" ,");
+            }
+            sb.append(String.format("%X", (int)chars[i]));
+        }
+        return sb.append("]").toString();
+    }
+
+
 }
