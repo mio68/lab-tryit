6373ebb6c5dfebd9c9dd0e284c70a35224f6c245
Igor Markivskiy <mio6823@gmail.com>
2023-01-18 18:39:28 +0700

    generics again

---
+++ src/main/java/mio68/lab/tryit/generics/WildCards.java
@@ -0,0 +1,53 @@
+package mio68.lab.tryit.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Java предоставляет особый вид параметризованного типа, именуемый ограниченным типом
+с символами подстановки (bounded wildcard type)
+
+Для максимальной гибкости для входных параметров,
+представляющих производителей или потребителей, следует использовать
+типы с символами подстановки.
+
+PECS — producer-extends, consumer-super
+(производитель — extends, потребитель — super)
+
+ */
+
+public class WildCards {
+
+    public static void main(String[] args) {
+
+        // ? extends -------------------------------------------
+        List<? extends Number> numExts; // It's produces subclasses of Number.
+                                        // It's not possible to put anything to it!
+        List<Integer> integers = new ArrayList<>(List.of(1,2,3));
+
+//        integers = numExts; // It' incorrect!
+        numExts = integers; // It's OK! numExts produces subclasses of Numbers,
+                            // and integers suits this case!
+        System.out.println( numExts.get(0)); // prints 1
+        Number aNum = numExts.get(1); // Compilation is Ok, so Runtime will be OK too, I think)
+
+//        numExts.add(new Object()); // compilation error
+//        numExts.add(Integer.valueOf(1)); // compilation error
+
+        // ? super -------------------------------------
+        List<? super Integer> intSupers; // Consumes an unknown class that is super classes of Integer,
+        // so you can add only Integers to collection.
+        // Lists that are able to write Integers are List<Object>, List<Number>, List<Integer>
+        // The only class than you can safely read from them is Object.
+
+//        integers = intSupers; // Not compatible! integers reads/writes exact integers!
+//        but from intSupers you can read Objects only.
+        intSupers = integers;
+        intSupers.add(1);
+//        intSupers.add(aNum); // compilation error. intSupers maybe a reference to List<Integer>!
+//        aNum = intSupers.get(1); // compilation error.
+         Object anObj = intSupers.get(1); // It's just OK!
+
+    }
+
+}
