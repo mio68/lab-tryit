a97d7a5f856eb9d468b77823ca4a4d04896279cf
Igor Markivskiy <mio6823@gmail.com>
2023-01-19 12:19:47 +0700

    generics, generics again!

--- src/main/java/mio68/lab/tryit/generics/WildCards.java
+++ src/main/java/mio68/lab/tryit/generics/WildCards.java
@@ -21,11 +21,13 @@ public class WildCards {
     public static void main(String[] args) {
 
         // ? extends -------------------------------------------
-        List<? extends Number> numExts; // It's produces subclasses of Number.
+        List<? extends Number> numExts = null; // It's produces subclasses of Number.
                                         // It's not possible to put anything to it!
         List<Integer> integers = new ArrayList<>(List.of(1,2,3));
 
 //        integers = numExts; // It' incorrect!
+//        List<Object> objs = numExts; // wildcard parametrized can'b assigned to parametrized
+        List lst = numExts; // It's only possible assigment
         numExts = integers; // It's OK! numExts produces subclasses of Numbers,
                             // and integers suits this case!
         System.out.println( numExts.get(0)); // prints 1
@@ -35,13 +37,16 @@ public class WildCards {
 //        numExts.add(Integer.valueOf(1)); // compilation error
 
         // ? super -------------------------------------
-        List<? super Integer> intSupers; // Consumes an unknown class that is super classes of Integer,
+        List<? super Integer> intSupers = null; // Consumes an unknown class that is super classes of Integer,
         // so you can add only Integers to collection.
         // Lists that are able to write Integers are List<Object>, List<Number>, List<Integer>
         // The only class than you can safely read from them is Object.
 
 //        integers = intSupers; // Not compatible! integers reads/writes exact integers!
 //        but from intSupers you can read Objects only.
+//        List<Object> objects = intSupers; // wildcard parametrized type as returned value - bad idea!
+          List list = intSupers; // Possible
+
         intSupers = integers;
         intSupers.add(1);
 //        intSupers.add(aNum); // compilation error. intSupers maybe a reference to List<Integer>!
