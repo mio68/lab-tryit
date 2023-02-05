dbddae25e5c568146cff6e390c19b05299f47023
Igor Markivskiy <mio6823@gmail.com>
2023-02-05 17:12:11 +0700

    BattleField check from codewars

--- src/main/java/mio68/lab/tryit/codewars/BattleField.java
+++ src/main/java/mio68/lab/tryit/codewars/BattleField.java
@@ -1,10 +1,123 @@
 package mio68.lab.tryit.codewars;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 // codewars https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/java
 public class BattleField {
 
     public static boolean fieldValidator(int[][] field) {
-        // your code here!
-        return false;
+        Field f = new Field(field);
+        Map<Ship, Integer> counterByShip = new HashMap<>();
+        // scan
+        for (int row = 0; row < field.length; row++) {
+            for (int col = 0; col < field.length; col++) {
+                if (f.isDiagonalCollisionAt(row, col)) {
+                    return false;
+                }
+
+                if (f.isLineStartFrom(row, col)) {
+                    Optional<Ship> ship = Ship.getShipByLength(f.getLineLengthFrom(row, col));
+                    if (ship.isEmpty()) {
+                        return false; // length doesn't match any ship
+                    }
+                    // count the ship
+                    counterByShip.compute(ship.get(),
+                            (ship1, count) -> count == null ? 1 : count + 1);
+                    if (isQuotaExceeded(counterByShip)) {
+                        return false;
+                    }
+
+                }
+            }
+        }
+        return isQuotaUsedCorrectly(counterByShip);
+    }
+
+    private static boolean isQuotaUsedCorrectly(Map<Ship, Integer> counterByShip) {
+        return counterByShip.size() == Ship.values().length &&
+                counterByShip.entrySet()
+                        .stream()
+                        .allMatch(entry -> entry.getValue() == entry.getKey().quota);
+    }
+
+    private static boolean isQuotaExceeded(Map<Ship, Integer> counterByShip) {
+        return counterByShip.entrySet()
+                .stream()
+                .anyMatch(entry -> entry.getValue() > entry.getKey().quota);
+    }
+
+    private enum Ship {
+        SUBMARINE(1, 4),
+        DESTROYER(2, 3),
+        CRUISER(3, 2),
+        BATTLESHIP(4, 1);
+
+        private final int length;
+        private final int quota;
+
+        Ship(int length, int quota) {
+            this.length = length;
+            this.quota = quota;
+        }
+
+        static Optional<Ship> getShipByLength(int len) {
+            return Arrays.stream(Ship.values())
+                    .filter(ship -> ship.length == len)
+                    .findAny();
+        }
+    }
+
+    private static class Field {
+        private final int[][] field;
+        private final int size;
+
+        Field(int[][] field) {
+            this.field = field;
+            this.size = field.length;
+        }
+
+        int getLineLengthFrom(int row, int col) {
+            boolean horizontalDirection = isHorizontalLineStartFrom(row, col);
+            int len = 0;
+            if (horizontalDirection) {
+                while (isOccupied(row, col++)) len++;
+            } else {
+                while (isOccupied(row++, col)) len++;
+            }
+            return len;
+        }
+
+        boolean isLineStartFrom(int row, int col) {
+            return isHorizontalLineStartFrom(row, col) || isVerticalLineStartFrom(row, col);
+        }
+
+        boolean isHorizontalLineStartFrom(int row, int col) {
+            return isEmpty(row, col - 1) && isOccupied(row, col)
+                    && isEmpty(row - 1, col) && isEmpty(row + 1, col);
+        }
+
+        boolean isVerticalLineStartFrom(int row, int col) {
+            return isEmpty(row - 1, col) && isOccupied(row, col)
+                    && isEmpty(row, col - 1) && isEmpty(row, col + 1);
+        }
+
+        boolean isDiagonalCollisionAt(int row, int col) {
+            return isOccupied(row, col)
+                    && (isOccupied(row + 1, col - 1) || isOccupied(row + 1, col + 1));
+        }
+
+        boolean isOccupied(int row, int col) {
+            return !isEmpty(row, col);
+        }
+
+        boolean isEmpty(int row, int col) {
+            if (row >= 0 && row < size && col >= 0 && col < size) {
+                return field[row][col] == 0;
+            }
+            return true;
+        }
     }
 }
--- src/test/java/mio68/lab/tryit/codewars/BattleFieldTest.java
+++ src/test/java/mio68/lab/tryit/codewars/BattleFieldTest.java
@@ -6,7 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BattleFieldTest {
 
-    private static int[][] battleField = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+    private static final int[][] validBattleField = {
+            {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
             {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
             {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -17,8 +18,94 @@ public class BattleFieldTest {
             {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
+    private static final int[][] battleFieldWithLongShip = {
+            {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 1, 1, 1, 1, 0, 0, 0, 0, 0}};
+
+    private static final int[][] battleFieldQuotaExceeded = {
+            {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 1, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    private static final int[][] battleFieldQuotaNotUsed = {
+            {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 0, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    private static final int[][] battleFieldBattleShipIsMissed = {
+            {0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    private static final int[][] invalidBattleField = {
+            {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 1, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
     @Test
-    public void SampleTest() {
-        assertEquals(true, BattleField.fieldValidator(battleField));
+    public void whenValidConfigurationAndQuota_thenOk() {
+        assertTrue(BattleField.fieldValidator(validBattleField));
     }
+
+    @Test
+    public void whenInvalidConfiguration_thenFail() {
+        assertFalse(BattleField.fieldValidator(invalidBattleField));
+    }
+
+    @Test
+    public void whenQuotaExceeded_thenFail() {
+        assertFalse(BattleField.fieldValidator(battleFieldQuotaExceeded));
+    }
+
+    @Test
+    public void whenQuotaNotUsed_thenFail() {
+        assertFalse(BattleField.fieldValidator(battleFieldQuotaNotUsed));
+    }
+
+    @Test
+    public void whenBattleShipIsMissed_thenFail() {
+        assertFalse(BattleField.fieldValidator(battleFieldBattleShipIsMissed));
+    }
+
+    @Test
+    public void whenLongShipFound_thenFail() {
+        assertFalse(BattleField.fieldValidator(battleFieldWithLongShip));
+    }
+
 }
