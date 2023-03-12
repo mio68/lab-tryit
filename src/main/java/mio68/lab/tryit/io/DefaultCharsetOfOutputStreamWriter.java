ca67abbc215154185a44a369240a4fef6e594a91
Igor Markivskiy <mio6823@gmail.com>
2023-03-12 16:52:52 +0700

    io and charsets

--- src/main/java/mio68/lab/tryit/io/DefaultCharsetOfOutputStreamWriter.java
+++ src/main/java/mio68/lab/tryit/io/DefaultCharsetOfOutputStreamWriter.java
@@ -3,6 +3,10 @@ package mio68.lab.tryit.io;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+
+// The charset that it uses may be specified by name or may be given explicitly,
+// or the platform's default charset may be accepted.
 
 public class DefaultCharsetOfOutputStreamWriter {
 
@@ -11,7 +15,9 @@ public class DefaultCharsetOfOutputStreamWriter {
         try (FileOutputStream fileOutputStream = new FileOutputStream("hello.txt");
              OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream)) {
 
-            outputStreamWriter.write(outputStreamWriter.getEncoding() + "\n");
+            System.out.printf("Default charset of this Java virtual machine: [%s]%n", Charset.defaultCharset());
+            System.out.printf("Default encoding for OutputStreamWriter: [%s]%n", outputStreamWriter.getEncoding());
+
             outputStreamWriter.write("Hello World!\n");
             outputStreamWriter.write("Привет Мир!\n");
         }
