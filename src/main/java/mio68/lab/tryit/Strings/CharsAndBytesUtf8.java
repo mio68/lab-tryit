358a06b85f2a2b6db92de9a4173fc5a4b732e470
Igor Markivskiy <mio6823@gmail.com>
2023-03-26 00:34:44 +0700

    UTF-8

--- src/main/java/mio68/lab/tryit/Strings/CharsAndBytesUtf8.java
+++ src/main/java/mio68/lab/tryit/Strings/CharsAndBytesUtf8.java
@@ -1,5 +1,6 @@
 package mio68.lab.tryit.Strings;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 
@@ -22,19 +23,19 @@ public class CharsAndBytesUtf8 {
 
     public static void main(String[] args) {
         String s = "a";
-        System.out.println(Arrays.toString(s.getBytes())); // prints [97]
+        System.out.println(Arrays.toString(s.getBytes(StandardCharsets.UTF_8))); // prints [97]
 
         char[] chars = new char[] {'\u0097'};
         // '\u0097' in range '\u0080' to '\u07FF' -> 2 bytes
         // 0x97 -> 0b10010111
         // 0b11000010, 0b10010111 -> -62, -105
         String str = new String(chars);
-        byte[] bytes = str.getBytes(); // UTF-8 uses one to four one-byte (8-bit) code units
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8); // UTF-8 uses one to four one-byte (8-bit) code units
         System.out.println(Arrays.toString(bytes)); // prints [-62, -105]
 
-        System.out.println(Arrays.toString(String.valueOf('\u007F').getBytes())); // [127]
-        System.out.println(Arrays.toString(String.valueOf('\u07FF').getBytes())); // [-33, -65]
-        System.out.println(Arrays.toString(String.valueOf('\uFFFF').getBytes())); // [-17, -65, -65]
+        System.out.println(Arrays.toString(String.valueOf('\u007F').getBytes(StandardCharsets.UTF_8))); // [127]
+        System.out.println(Arrays.toString(String.valueOf('\u07FF').getBytes(StandardCharsets.UTF_8))); // [-33, -65]
+        System.out.println(Arrays.toString(String.valueOf('\uFFFF').getBytes(StandardCharsets.UTF_8))); // [-17, -65, -65]
 
     }
 }
