e9ee9c4248365270defea4c74034e4b94bab51e3
Igor Markivskiy <mio6823@gmail.com>
2023-03-26 00:29:19 +0700

    NaN

--- src/main/java/mio68/lab/tryit/realnumbers/NanComparing.java
+++ src/main/java/mio68/lab/tryit/realnumbers/NanComparing.java
@@ -4,6 +4,6 @@ public class NanComparing {
 
     public static void main(String[] args) {
         System.out.println(Float.NaN == Float.NaN); // Prints false!
-        // If you wand to check if float or double is NaN use Float.isNaN(x) or Double.isNaN(x)
+        // If you want to check if float or double is NaN use Float.isNaN(x) or Double.isNaN(x)
     }
 }
