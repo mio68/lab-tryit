143856159bbb0c5f18d27734987443f1bf262496
Igor Markivskiy <mio6823@gmail.com>
2023-03-25 23:00:29 +0700

    dividing by 0.0 gives Infinity

---
+++ src/main/java/mio68/lab/tryit/realnumbers/Infinity.java
@@ -0,0 +1,9 @@
+package mio68.lab.tryit.realnumbers;
+
+public class Infinity {
+
+    public static void main(String[] args) {
+        System.out.println(1.0 / 0.0); // Prints Infinity
+    }
+
+}
