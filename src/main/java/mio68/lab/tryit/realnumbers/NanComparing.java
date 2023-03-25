65d77f6403e8a220e9ad74e08fa90c3060174e72
Igor Markivskiy <mio6823@gmail.com>
2023-03-25 23:02:27 +0700

    NaN

--- src/main/java/mio68/lab/tryit/realnumbers/NanComparing.java
+++ src/main/java/mio68/lab/tryit/realnumbers/NanComparing.java
@@ -3,6 +3,7 @@ package mio68.lab.tryit.realnumbers;
 public class NanComparing {
 
     public static void main(String[] args) {
-        System.out.println(Float.NaN == Float.NaN); // Prints false
+        System.out.println(Float.NaN == Float.NaN); // Prints false!
+        // If you wand to check if double is NaN use Double.isNaN(x)
     }
 }
