2c80fb7ef2b53618b52bc88e3b28f77bbd3c9e6a
Igor Markivskiy <mio6823@gmail.com>
2023-04-25 16:49:17 +0700

    lombok value with collection solved

---
+++ src/main/java/mio68/lab/tryit/lombok/ValueAndBuilder.java
@@ -0,0 +1,49 @@
+package mio68.lab.tryit.lombok;
+
+import lombok.Builder;
+import lombok.Singular;
+import lombok.Value;
+import lombok.With;
+
+import java.util.Collections;
+import java.util.Set;
+
+@Value
+@Builder(toBuilder = true)
+class Person {
+
+    String name;
+    @Singular
+    @With
+    Set<String> skills;
+
+}
+
+
+public class ValueAndBuilder {
+
+    public static void main(String[] args) {
+        Person jonh = Person.builder()
+                .name("John")
+                .build();
+        System.out.println(jonh);
+
+        Person johnUpgraded = jonh.toBuilder()
+                .skill("Java")
+                .skill("Spring")
+                .build();
+        System.out.println(johnUpgraded);
+
+//        Person johnDowngraded = johnUpgraded.toBuilder()
+//                .skills(Collections.emptySet())
+//                .build();
+
+        Person johnDowngraded = johnUpgraded.toBuilder()
+                .build()
+                .withSkills(Collections.emptySet());
+
+        System.out.println(johnDowngraded);
+
+    }
+
+}
