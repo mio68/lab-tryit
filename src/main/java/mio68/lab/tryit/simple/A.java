9af3e15a6c62e8aee7b3cb54b09bb7def759451f
Igor Markivskiy <mio6823@gmail.com>
2023-09-12 10:58:43 +0300

    simple classes for JVM learning

---
+++ src/main/java/mio68/lab/tryit/simple/A.java
@@ -0,0 +1,19 @@
+package mio68.lab.tryit.simple;
+
+public class A {
+    private int id;
+    private String name;
+
+    public A(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/mio68/lab/tryit/simple/Test.java
@@ -0,0 +1,10 @@
+package mio68.lab.tryit.simple;
+
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        A a = new A(123, "Darth Vader");
+        System.in.read();
+    }
+}
