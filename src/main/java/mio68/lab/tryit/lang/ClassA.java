e8cb2ac5f0ff28b80ff9c1b2565916d8e58b19de
Igor Markivskiy <mio6823@gmail.com>
2023-10-02 20:32:01 +0300

    one top-level class per file

---
+++ src/main/java/mio68/lab/tryit/lang/ClassA.java
@@ -0,0 +1,12 @@
+package mio68.lab.tryit.lang;
+
+// Comment to Josua Bloch 4.11 Ограничивайтесь одним классом
+//верхнего уровня на исходный файл
+
+public class ClassA {
+}
+
+// It's not possible to define two top-level classes in the same file
+//public class ClassB {
+//
+//}
